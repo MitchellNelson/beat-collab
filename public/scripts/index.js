56f219c7c39ba1ecd503f184624d66e839036b14
Mitchell Nelson <nels4929@stthomas.edu>
2019-05-21 20:45:14 -0500

    Play/Pause Websocket messages

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -11,7 +11,8 @@ function Init()
             curr_note_index:0,
             prev_note_index:15,
             bpm: 100,
-            playing: true,
+            playing: false,
+            user_num:null,
             timer: null
         },
         watch: {
@@ -28,11 +29,8 @@ function Init()
                 }
             }
         }
-
 	});
 
-    
-   
     CreateDrum("Kick", "../audio/Kick.mp3");
     CreateDrum("Snare", "../audio/Snare.mp3");
     CreateDrum("Hihat", "../audio/Hat.mp3");
@@ -52,51 +50,35 @@ function Init()
     	var message = JSON.parse(event.data);
         console.log(message);
 		if(message.msg === "client_count"){
-            console.log("got a client_count msg")
+            app.user_num=message.data;
+        }
+        else if(message.msg === "play"){
+            app.playing = !app.playing;
+            //Play();
+        }
+        else if(message.msg === "stop"){
+            app.playing = !app.playing;
+            //Stop();
         }
     };
-    Play();
 }
 
-//interval.js - https://gist.github.com/manast/1185904 
-function interval(duration, fn){
-    this.baseline = undefined
-  
-    this.run = function(){
-    if(this.baseline === undefined){
-        this.baseline = new Date().getTime()
-    }
-    fn()
-    var end = new Date().getTime()
-    this.baseline += duration
- 
-    var nextTick = duration - (end - this.baseline)
-    if(nextTick<0){
-        nextTick = 0
-    }
-    (function(i){
-        i.timer = setTimeout(function(){
-        i.run(end)
-      }, nextTick)
-    }(this))
+function ClickPlayButton(){ 
+    ws.send(JSON.stringify({'msg': 'play'}));
 }
 
-this.stop = function(){
-   clearTimeout(this.timer)
- }
+function ClickStopButton(){
+    ws.send(JSON.stringify({'msg': 'stop'}));
 }
 
 function Play(){
-    //var app.curr_note_index = 0;
-    //var app.prev_note_index = 15;
     app.timer = new interval(60000/app.bpm/4, function(){
         for(var j = 0; j<app.all_rows.length; j++){
-            app.all_rows[j].nodes[app.curr_note_index].curr_note = true;
-            app.all_rows[j].nodes[app.prev_note_index].curr_note = false;
             if(app.all_rows[j].nodes[app.curr_note_index].play == true ){
                 app.all_rows[j].nodes[app.curr_note_index].audio_element.play();
             }
-
+            app.all_rows[j].nodes[app.curr_note_index].curr_note = true;
+            app.all_rows[j].nodes[app.prev_note_index].curr_note = false;
         }
         app.prev_note_index=app.curr_note_index;
         app.curr_note_index = (app.curr_note_index + 1) % 16;
@@ -111,13 +93,12 @@ function Stop(){
 function drum_row(name, audio_path){
     this.inst = name;
     this.nodes = [];
-        var sound = new Howl({
-            src: [audio_path]
-        });
+    var sound = new Howl({
+        src: [audio_path]
+    });
     //initialize elements to all false 
     //initialize all 16 cloned audio players
     for(var i = 0; i<num_counts * 4; i++){
-
         var node_entry = {curr_note: false, selected: false, play: false, audio_element: sound};
         this.nodes.push(node_entry);
     }
